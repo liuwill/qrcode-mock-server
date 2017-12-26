3000a957f40ebb3281f6c16ef7386b77e1eac3fb
liuwill <liuwei_will@qq.com>
2017-12-26 17:29:18 +0800

    change fold structure

--- pages/_document.js
+++ pages/_document.js
@@ -3,23 +3,23 @@ import CommonStyle from '../styles/common.scss'
 
 class MyDocument extends Document {
   render() {
-      const { nextStyle } = this.props;
+    const { nextStyle } = this.props;
 
-      return (
-        <html>
-          <Head>
-            <meta name='viewport' content='width=device-width, initial-scale=1' />
-            <meta charSet='utf-8' />
-            <link rel='stylesheet' href='/static/styles/semantic.min.css' />
-            <link rel='stylesheet' href='/static/css/bundle.css' />
-            {/* <style dangerouslySetInnerHTML={{ __html: CommonStyle }} /> */}
-          </Head>
-          <body className={CommonStyle['demo-body']}>
-            <Main />
-            <NextScript />
-          </body>
-        </html>
-      );
+    return (
+      <html>
+        <Head>
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <meta charSet='utf-8' />
+          <link rel='stylesheet' href='/static/styles/semantic.min.css' />
+          <link rel='stylesheet' href='/static/assets/bundle.css' />
+          {/* <style dangerouslySetInnerHTML={{ __html: CommonStyle }} /> */}
+        </Head>
+        <body className={CommonStyle['demo-body']}>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 
   static getInitialProps ({ renderPage }) {
